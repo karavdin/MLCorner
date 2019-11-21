132d5be26abe605106059e0a0b6f586de1781447
Anastasia Karavdina <anastasia.karavdina@desy.de>
2019-11-21 11:05:03 +0100

    change input processing files for Tstar analysis

--- InputsPreparation/UHH2toNumpy/steer.py
+++ InputsPreparation/UHH2toNumpy/steer.py
@@ -16,15 +16,16 @@ ModuleRunner = ModuleRunner(path_MLDIR, outpath)
 
 # ---- Macro for ML inputs preparation ----
 #names of the process, e.g part after uhh2.AnalysisModuleRunner. in the input file name
-procnames = ['TTbar', 'QCD_Mu', 'ST', 'DYJets', 'WJets', 'RSGluon', 'RSGluon_M1000', 'RSGluon_M2000', 'RSGluon_M3000', 'RSGluon_M4000', 'RSGluon_M5000', 'RSGluon_M6000']
+procnames = ['TTbar', 'ST', 'DYJets', 'WJets', 'TstarTstarToTgluonTgluon', 'TstarTstarToTgammaTgluon_M-1500', 'TstarTstarToTgammaTgluon_M-1600', 'TstarTstarToTgluonTgluon_M-700',
+             'TstarTstarToTgluonTgluon_M-800','TstarTstarToTgluonTgluon_M-900','TstarTstarToTgluonTgluon_M-1100','TstarTstarToTgluonTgluon_M-1200','TstarTstarToTgluonTgluon_M-1300',
+             'TstarTstarToTgluonTgluon_M-1600']
 #name of branches to be skipped in conversion
 ## exact names
-unwanted_exact_tags = ['n_ele', 'n_mu']
+unwanted_exact_tags = []
 ## partial names to exclude common set of variables at once
-unwanted_tags = ['weight_', 'jet4', 'jet5', 'jet6', 'jet7', 'jet8', 'jet9', 'jet10', 'N_bJets_', '_zprime', 'eventweight', 
-                  '_ele1', 'tau32', 'tau21','e_jet','px_','py_','e_ak8','met_p','pz_']
+unwanted_tags = []
 
-syst_vars = ['NOMINAL','JEC_up','JEC_down','JER_down','JER_up']
+syst_vars = ['NOMINAL']
 for syst_var in syst_vars:
     print'--- Convert inputs for ',syst_var,' variation ---'
     ModuleRunner.ReadoutMLVariables(procnames=procnames,unwanted_tags=unwanted_tags, unwanted_exact_tags=unwanted_exact_tags,syst_var=syst_var)
